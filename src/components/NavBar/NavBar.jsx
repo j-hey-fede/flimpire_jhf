840f5310404d9aabee2a8f5a4c61499abee215a2
Jacquelyn <apps.jhf@gmail.com>
2022-12-05 07:51:02 -0500

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { useTheme } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
 const NavBar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
+
   return (
-    <div>NavBar</div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={()=>{}}
+              className={classes.menubutton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={()=> {}}>
+              {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick= {() => {}} >
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ): (
+              <Button
+                color="inherit"
+                component={Link}
+                to='/profile/:id'
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar 
+                  style={{ width: 30, height: 30 }} 
+                  alt="profile" 
+                  src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.eyhIau9Wqaz8_VhUIomLWgHaHa%26pid%3DApi%26h%3D160&f=1&ipt=7526256e4922a063c75d605e1b7d9d3c07b8c8e9c6398fa47511abbc2eb8106b&ipo=images"
+
+                  />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+    </>
   )
 }
 
