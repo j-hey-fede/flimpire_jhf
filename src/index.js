dd72e5778f7c93abe319994f5d3be8c6787886ca
Jacquelyn <apps.jhf@gmail.com>
2022-12-05 17:41:29 -0500

    api connect

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, Proviider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ThemeProvider>,
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </Provider>,
     
     document.getElementById('root'));
