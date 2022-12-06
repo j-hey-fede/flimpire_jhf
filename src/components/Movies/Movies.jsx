7e4e9c3b85ec6e35ae4835ef598edc15135f402e
Jacquelyn <apps.jhf@gmail.com>
2022-12-06 12:25:31 -0500

    render movies images

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,14 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
+import useStyles from './styles';
+
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const classes = useStyles();
+  const { data, error, isFetching } = useGetMoviesQuery();
   console.log(data);
 
+  if(isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+    
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies match that search.
+          <br/>
+          Please search for something else!
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occurred.'
+  
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data}/>
+    </div>
   )
 }
 
